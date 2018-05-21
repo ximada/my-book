96818a9a691c5c0306dec21eaa2164ed3d12f3cf
Adalid <witch_ada@hotmail.com>
2018-05-20 23:22:35 -0500

    Creando my book

---
+++ js/app.js
@@ -0,0 +1,34 @@
+
+
+//efeto maquina de escribir
+
+var mensage1 = "Hi! Soy Ada Valdovinos", pausa = 50; 
+var mensage2 = "Front End Developer", pausa = 50; 
+function start(){ 
+ var i = 0;
+ var m = mensage1.split(''); 
+ var t = setInterval( 
+ function(){ 
+ if(i >= m.length-1)clearInterval(t); 
+ document.getElementById('efect', ).innerHTML+=m[i];
+ i++; 
+ }, pausa); 
+} 
+start(); 
+function secondText(){ 
+    var i = 0;
+    var m = mensage2.split(''); 
+    var t = setInterval( 
+    function(){ 
+    if(i >= m.length-1)clearInterval(t); 
+    document.getElementById('efect2', ).innerHTML+=m[i];
+    i++; 
+    }, pausa); 
+   } 
+   secondText();
+
+//menu hamburgesa
+$('.hamburger').click(function () {
+    $('.hamburger').toggleClass('open');
+});
+
